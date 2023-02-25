4de6342f6b0ba3cb26c8f3fd0b6ab4ae343cbcd1
ReylordDev <98845999+ReylordDev@users.noreply.github.com>
2023-02-26 00:55:20 +0100

    E06 - street names progress

--- lectures/makemore/part1/E06_dataset_preprocessing.py
+++ lectures/makemore/part1/E06_dataset_preprocessing.py
@@ -1,13 +1,31 @@
 import csv
 
-streetnames: set = set()
+streetnames = set()
+
+UNWANTED_CHARS = ['.', '1', '2', '3', '4',
+                  '5', '6', '7', '8', '9', '0', '+', ',', '\"', '_', '`', '/']
+
+REPLACABLE_CHARS = {'á': 'a', 'â': 'a', 'è': 'e',
+                    'é': 'e', 'ë': 'e', 'í': 'i', 'ó': 'o'}
 
 with open('./lectures/makemore/germany.csv', encoding='utf-8') as csvfile:
     reader = csv.DictReader(csvfile)
     print(reader.fieldnames)
     for row in reader:
-        streetnames.add(row['STREET'])
+        streetname = row['STREET'].lower()
+        if streetname in streetnames:
+            continue
+        if any(char in streetname for char in UNWANTED_CHARS):
+            continue
+        for char in REPLACABLE_CHARS:
+            streetname = streetname.replace(char, REPLACABLE_CHARS[char])
+        # remove brackets and everything inside from streetname
+        if '(' in streetname:
+            streetname = streetname[:streetname.index('(')]
+        streetnames.add(streetname)
 
 with open('./lectures/makemore/part1/street-names.txt', 'w', encoding='utf-8') as write_file:
     for streetname in streetnames:
         write_file.write(streetname + '\n')
+
+print(f'Done!, wrote {len(streetnames)} street names to file.')
